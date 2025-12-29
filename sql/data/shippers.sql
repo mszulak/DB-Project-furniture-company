8a5571d99522ac82fc2ee5d96b5169789700af75
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-12-29 15:40:22 +0100

    feat: more sample data

---
+++ sql/data/shippers.sql
@@ -0,0 +1,12 @@
+USE test_db;
+
+insert into Shippers (id, shipper_name) values (1, 'Murphy LLC');
+insert into Shippers (id, shipper_name) values (2, 'MacGyver, Kuhic and Lebsack');
+insert into Shippers (id, shipper_name) values (3, 'Marquardt Group');
+insert into Shippers (id, shipper_name) values (4, 'Lockman, Carter and Swift');
+insert into Shippers (id, shipper_name) values (5, 'McKenzie, Osinski and Rolfson');
+insert into Shippers (id, shipper_name) values (6, 'Green Inc');
+insert into Shippers (id, shipper_name) values (7, 'Glover-Powlowski');
+insert into Shippers (id, shipper_name) values (8, 'Haley, Beahan and Koepp');
+insert into Shippers (id, shipper_name) values (9, 'Padberg-Douglas');
+insert into Shippers (id, shipper_name) values (10, 'Wuckert, Ernser and Lockman');
